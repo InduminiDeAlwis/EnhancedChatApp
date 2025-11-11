f881c460c1dfa6b6aa7a31a62818eae2baf2ddc7
InduminiDeAlwis <dealwiskkit.22@uom.lk>
2025-11-11 15:13:24 +0530

    Add Constants, Message, and MessageType classes for chat application

---
+++ src/common/Constants.java
@@ -0,0 +1,10 @@
+package common;
+
+/**
+ * Application constants (server address/port etc.)
+ */
+public class Constants {
+	// Default server host and port used by the client and server
+	public static final String SERVER_IP = "127.0.0.1";
+	public static final int SERVER_PORT = 12345;
+}
---
+++ src/common/MessageType.java
@@ -0,0 +1,11 @@
+package common;
+
+/**
+ * Types of messages exchanged between client and server.
+ */
+public enum MessageType {
+	LOGIN,
+	LOGOUT,
+	BROADCAST,
+	PRIVATE
+}
